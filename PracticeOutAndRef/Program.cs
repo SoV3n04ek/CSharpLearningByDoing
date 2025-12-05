9cf405ebec23c616a10ef8bb89be1e320ce79266
SoV3h04ek <mr.ivan121212@gmail.com>
2025-12-05 21:00:14 +0100

    Initial commit with project structure and GitIgnore

---
+++ PracticeOutAndRef/Program.cs
@@ -0,0 +1,108 @@
+﻿/*
+ * The ref keyword indicates that an argument 
+ * is being passed by reference and must be 
+ * initialized before it is passed to the method.
+ * Purpose: To allow a method to read and modify the value of an argument, and have that modification reflected in the calling code.
+
+Requirement: The variable must have an initial value before being passed to the method.
+ * */
+
+void IncrementValue(ref int number)
+{
+    number = number + 10;
+}
+void IncrementValueWithoutRef(int number)
+{
+    number = number + 10;
+}
+
+// example
+/*
+int a = 5;
+IncrementValueWithoutRef(a);
+Console.WriteLine(a);
+
+IncrementValue(ref a);
+Console.WriteLine(a);
+*/
+
+
+/* 
+ * The out keyword indicates that an argument 
+ * is being passed by reference, but the method 
+ * is responsible for assigning a value to the parameter 
+ * before the method returns.
+
+Purpose: To allow a method to return multiple values 
+(in addition to its normal return type).
+
+Requirement: The variable does not need to be initialized 
+before being passed. The method must assign 
+a value to the out parameter before it exits.
+*/
+
+bool Divide(int numerator, int denominator, out int result)
+{
+    if (denominator != 0)
+    {
+        result = numerator / denominator;
+        return true;
+    }
+    else
+    {
+        result = 0;
+        return false;
+    }
+}
+
+// Usage (since C# 7, you can declare the out variable inline):
+if (Divide(10, 2, out int quotient))
+{
+    Console.WriteLine(quotient);
+}
+// Note: 'quotient' is now scoped to the outer block.
+// It means: The variable DOES NOT disappear after the if statement!
+
+
+void TestingRefOutProcessor()
+{
+    int x = 2, y = 6;
+    Console.WriteLine($"x {x} y {y} ");
+    RefOutProcessor.SwapIntegers(ref x, ref y);
+
+    int[] tableOfData = { 4, 2, 3, 25, 74, 3, 2, 454, 23, 4, 534, 3 };
+    RefOutProcessor.CalculateStatistics(tableOfData, out int sum, out double average);
+    Console.WriteLine($"Sum: {sum} average: {average}");
+
+}
+
+TestingRefOutProcessor();
+
+class RefOutProcessor
+{
+    public static void SwapIntegers(ref int x1, ref int x2)
+    {
+        int temp = x1;
+        x1 = x2;
+        x2 = temp;
+    }
+
+    public static bool CalculateStatistics(int[] numbers, out int sum, out double avarage)
+    {
+        sum = 0;
+        avarage = 0;
+
+        foreach (int number in numbers)
+        {
+            sum += number;
+            avarage += number;
+        }
+
+        if (numbers.Length != 0)
+            avarage /= numbers.Length;
+        else
+            avarage = 0;
+
+        return true;
+    }   
+}
---
+++ ThreadsPractice/Program.cs
@@ -0,0 +1,57 @@
+﻿public class TruckingOperation
+{
+    // the shared resource limit: 10 parking places.
+    private const int ParkingPlacesCount = 10;
+    private static readonly SemaphoreSlim _parkingSpots = new SemaphoreSlim(ParkingPlacesCount);
+    private const int TotalTrucks = 100;
+    private const int UnloadTimeMs = 3500; // 3 seconds per truck
+
+    public static void RunExample()
+    {
+        Console.WriteLine($"*** Starting Trucking Operation with {TotalTrucks} trucks and {ParkingPlacesCount} parking spots for unloading ***");
+
+        // 1. Create 100 tasks (one for each truck)
+        Task[] truckTasks = new Task[TotalTrucks];
+
+        for (int i = 0; i < TotalTrucks; i++)
+        {
+            int truckId = i + 1; // Unique id for each truck
+            truckTasks[i] = Task.Run(() => UnloadTruck(truckId));
+        }
+
+        // 2. Wait for all tasks to complete 
+
+        Console.WriteLine("\n*** All trucks have been unloaded. Operation Complete.");
+        Console.ReadLine();
+    }
+
+    private static void UnloadTruck(int truckId)
+    {
+        Console.WriteLine($"Truck {truckId} is approaching the parking lot.");
+
+        // wait to acquire a spot (blocks if 3 are in use)
+        _parkingSpots.Wait(); // blocks current block until it can enter a semaphore
+
+        try
+        {
+            Console.WriteLine($"\t[PARKED] Truck {truckId} is unloading. AvaibleSpots: {_parkingSpots.CurrentCount}");
+
+            // Simulate 3 seconds unloading time
+            Thread.Sleep(UnloadTimeMs);
+
+            Console.WriteLine($"\t[EXITED] Truck {truckId} finished unloading");
+        }
+        finally
+        {
+            _parkingSpots.Release();
+        }
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        TruckingOperation.RunExample();
+    }
+}
